c7cfcc9c2412f907d700f5b9c368ac249ecc2434
Alex <nesterov178@gmail.com>
2022-06-26 23:26:57 +0300

    binding adapter

--- app/src/main/java/com/frigate/appselectmovie/presentation/MainActivity.kt
+++ app/src/main/java/com/frigate/appselectmovie/presentation/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.frigate.appselectmovie.databinding.ActivityMainBinding
 import com.frigate.appselectmovie.domain.MovieUnit
@@ -27,23 +28,47 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         view = ViewModelProvider(this).get(MainViewModel::class.java)
+        setupRecyclerView()
         lifecycleScope.launch {
-            setupRecyclerView(offset)
+            fillAdapter(offset)
         }
         binding.buttonAddShopItem.setOnClickListener {
             offset += 20
             lifecycleScope.launch {
-                setupRecyclerView(offset)
+                setupRecyclerView()
             }
         }
 
+        val layoutManager = binding.movieList.layoutManager as LinearLayoutManager
+        binding.movieList.addOnScrollListener(object: RecyclerView.OnScrollListener(){
+
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+
+                val totalItem = layoutManager.itemCount
+                val lastVisiblePosition = layoutManager.findLastVisibleItemPosition()
+                if(!adapter.isLoading && totalItem <=
+                        lastVisiblePosition + 10) {
+                    adapter.loadMore?.onLoadMore()
+                }
+            }
+        })
+        adapter.loadProcess (object : MovieListAdapter.MyLoader {
+            override fun onLoadMore() {
+                offset += 20
+                lifecycleScope.launch {
+                    fillAdapter(offset)
+                }
+            }
+        })
     }
 
-    private suspend fun setupRecyclerView(offset: Int) {
-
+    private fun setupRecyclerView() {
         adapter = MovieListAdapter()
         binding.movieList.adapter = adapter
+    }
 
+    fun fillAdapter(offset: Int) {
         GlobalScope.launch(Dispatchers.Main) {
             view.getMovieListFromVM(offset).observe(this@MainActivity) {
                 adapter.list = it as MutableList<MovieUnit>
--- app/src/main/java/com/frigate/appselectmovie/presentation/MovieListAdapter.kt
+++ app/src/main/java/com/frigate/appselectmovie/presentation/MovieListAdapter.kt
@@ -3,17 +3,17 @@ package com.frigate.appselectmovie.presentation
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.frigate.appselectmovie.R
-import com.frigate.appselectmovie.databinding.ActivityMainBinding
 import com.frigate.appselectmovie.databinding.ItemLayoutBinding
 import com.frigate.appselectmovie.domain.MovieUnit
 
 class MovieListAdapter : RecyclerView.Adapter<MovieListAdapter.MovieUnitViewHolder>() {
-    private lateinit var bindding: ItemLayoutBinding
+    private lateinit var binding: ItemLayoutBinding
+    var isLoading = false
+    var loadMore : MyLoader? = null
+
+
 
     var list = mutableListOf<MovieUnit>()
         set(value) {
@@ -32,21 +32,42 @@ class MovieListAdapter : RecyclerView.Adapter<MovieListAdapter.MovieUnitViewHold
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieUnitViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-        bindding = ItemLayoutBinding.inflate(inflater, parent, false)
-        return MovieUnitViewHolder(bindding.root, bindding)
+        binding = ItemLayoutBinding.inflate(inflater, parent, false)
+        return MovieUnitViewHolder(binding.root, binding)
     }
 
     override fun onBindViewHolder(holder: MovieUnitViewHolder, position: Int) {
         holder.bind(list[position])
-        updateAdapter(list)
-
     }
 
     override fun getItemCount(): Int {
         return list.size
     }
 
-    fun updateAdapter(list: List<MovieUnit>) {
+    fun updateAdapter(list: MutableList<MovieUnit>) {
+        list.clear()
+        list.addAll(list)
+        notifyDataSetChanged()
+    }
 
+
+
+    fun loadProcess(loadMore: MyLoader?) {
+        this.loadMore = loadMore
+    }
+
+    fun startLoading() {
+        isLoading = true
+    }
+
+    fun setLoaded() {
+        isLoading = false
+    }
+
+    interface MyLoader {
+        fun onLoadMore()
+    }
+    fun fillAdapter(){
+        if (list.isNotEmpty()) updateAdapter(list)
     }
 }
